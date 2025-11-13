8cb771bae54ba3da6209559fbab03aecd78b877b
199520gayu-ops <199520gayu@gmail.com>
2025-11-13 21:49:38 +0530

    Your descriptive commit message

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Import global styles (optional)
 
-
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Use ReactDOM.createRoot for React 18 concurrent features
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* The main component of your application */}
     <App />
-      
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
